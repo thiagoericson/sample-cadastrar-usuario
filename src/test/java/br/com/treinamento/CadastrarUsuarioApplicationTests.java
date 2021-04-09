afb2a48f742acceb19dba1bc38a5406aa9dcd075
Thiago Cabral <ericson.thiago@gmail.com>
2021-04-09 15:51:55 -0300

    First commit

---
+++ src/test/java/br/com/treinamento/CadastrarUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrarUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
